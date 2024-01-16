547b20c60b39a9557632a71e11f9d4a171a75d60
Audry19 <audrybarboza10@gmail.com>
2024-01-16 16:14:44 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var yesButton = document.getElementsByClassName("btn");
 yesButton[0].addEventListener("click", confirmButton);
 function confirmButton()
 {
-    document.querySelector("h1").innerHTML = "Thank you Babe!😘 <br>Cya at 6:00pm today😉. Get some chocolate.🍫</br>";
+    document.querySelector("h1").innerHTML = "Thank you Babe!😘 <br>Cu at 6:30pm today😉. Get some chocolate.🍫</br>";
     document.querySelector("img").src = "./Images/milk-and-mocha-thank-you.gif";
     yesButton[0].style.visibility = 'hidden';
     circle[0].style.visibility = 'hidden';
